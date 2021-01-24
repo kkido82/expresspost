358618c47724edd44964c5a7600da71b8f52be2b
Your Name <you@example.com>
2021-01-24 20:29:05 +0200

    add router

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,41 @@
 const express = require('express');
 const usersRouter = express.Router();
+const users = [{
+    id: 1,
+    name: 'Ido',
+}, {
+    id: 2,
+    name: 'Ronit',
+}, {
+    id: 3,
+    name: 'Danielle',
+}, {
+    id: 4,
+    name: 'Noam',
+},];
 
-usersRouter.route('/').get(function(req, res){
-    const data = req.body;
+usersRouter.route('/users')
+    .get(function (req, res) {
+        // const data = req.body;
 
-    res.json(['Ido', 'Ronit', 'Danielle', 'Noam']);
-});
+        res.json(users);
+    }).post(function (req, res) {
+        const user = JSON.parse(req.body.user);
+        user.id = users.length + 1;
+
+        users.push(user);
+        res.json(user);
+    });
+
+usersRouter.route('/users/:user_id')
+    .get(function (req, res) {
+        const user = users.find(u => u.id === +req.params.user_id);
+
+        res.json(user);
+    }).put(function(req, res) {
+        users.filter(u => u.id === +req.params.user_id).map(u => u.name = req.body.name);
+
+        res.json(users);
+    });
 
 module.exports = usersRouter;
--- server.js
+++ server.js
@@ -1,20 +1,41 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
+const path = require('path');
+
 const postRoute = require('./routes/post');
 const usersRoute = require('./routes/users');
 
 const app = express();
+const router = express.Router();
 const port = process.env.port || 3000;
 
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-
 app.use(bodyParser.json());
 
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+// Add headers
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', ['X-Requested-With,content-type']);
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
     next();
 });
 
@@ -25,11 +46,23 @@ app.use(function (req, res, next) {
 app.get('/', function (req, res, next) {
     // console.log('get is working');
 
-    res.json({ success: true, message: 'API is working' });
+    res.json({ success: true, message: 'API is working!' });
 });
 
-app.use('/get-report', postRoute);
-app.use('/users', usersRoute);
+// app.use('/get-report', postRoute);
+// app.use(['/users', '/user'], usersRoute);
+router.use(usersRoute);
+router.use('/post', postRoute);
+
+// router.route(['/users', '/u']).get(function(req,res) {
+//     res.json("a");
+// });
+
+
+
+
+
+app.use('/api', router);
 
 app.listen(port, () => {
     console.log('Server is running on port:', port);
